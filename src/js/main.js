d4a408d501df81755ec0927e94421ec5567504ca
Patrick Martinus <trickmartpat77@gmail.com>
2021-03-02 17:25:59 +0700

    fixing loader bug

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,64 @@
-window.addEventListener("load", () => {
-  document.querySelector("#loader").style.display = 'none';
-  console.log(window.location.hash);
-  let pageChange = window.location.hash;
-  onRouteChanged(pageChange);
+document.addEventListener("load", () => {
+  console.log(window.location);
+  if (window.location.hash == "") loadPage("home");
 });
 
 // Sidebar config
 document.addEventListener("DOMContentLoaded", () => {
   const elems = document.querySelector(".sidenav");
   let options = {
-    edge: 'left',
+    edge: "left",
     dragable: true,
-    preventScrolling: true
-  }
+    preventScrolling: true,
+  };
   let instances = M.Sidenav.init(elems, options);
-  
-  document.querySelectorAll(".topnav li a, .sidenav li a").forEach(elm => {
-    elm.addEventListener("click", event => {
-      const instance = M.Sidenav.getInstance(elems)
+
+  document.querySelectorAll(".topnav li a, .sidenav li a").forEach((elm) => {
+    elm.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      document.querySelector("#loader").style.display = "block";
+
+      const page = event.target.getAttribute("href");
+      loadPage(page);
+
+      const instance = M.Sidenav.getInstance(elems);
       instance.close();
-    })
-  })
+    });
+  });
 });
 
-function loadPage(page){
-  fetch(`${page}`)
-  .then(res => {
-    return res.text();
-  })
-  .then(html => {
+function loadPage(direction) {
+  // Define the path
+  let hash = direction;
+  let page = "";
 
-    // convert the html string into a document object
-    /*const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');*/
-    const bodyContent = document.querySelector("#body-content")
-    bodyContent.innerHTML = html;
-  })
-  .catch(err => console.error(err));
-}
-
-function onRouteChanged(pageChange){
-  const hash = window.location.hash;
-  let page = '';
-
-  switch(hash || pageChange){
-    case '#about': page = '/pages/about/about.html';
-    break;
-    case '#portofolio': page = 'pages/portofolio/portofolio.html';
-    break;
-    case '#blog': page = '/pages/blogs/blog.html';
-    break;
-    case '#chitchat': page = '/pages/chitchat/chitchat.html';
-    break;
-    case '#home': page = '/pages/home/home.html';
-    break; 
-    default: page = '/pages/home/home.html';
-    break;
+  switch (hash) {
+    case "#about":
+      page = "/pages/about/about.html";
+      break;
+    case "#portofolio":
+      page = "pages/portofolio/portofolio.html";
+      break;
+    case "#blog":
+      page = "/pages/blogs/blog.html";
+      break;
+    case "#chitchat":
+      page = "/pages/chitchat/chitchat.html";
+      break;
+    case "#home":
+      page = "/pages/home/home.html";
+      break;
   }
 
-  loadPage(page);
-
+  fetch(`${page}`)
+    .then((res) => {
+      return res.text();
+    })
+    .then((html) => {
+      const bodyContent = document.querySelector("#body-content");
+      bodyContent.innerHTML = html;
+      document.querySelector("#loader").style.display = "none";
+    })
+    .catch((err) => console.error(err));
 }
-
-window.addEventListener("hashchange", onRouteChanged);
